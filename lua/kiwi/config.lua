cd2fc772af6926fa3b0bae836e3947c4bfacd232
serenevoid <ajuph9224@gmail.com>
2023-07-19 18:09:38 +0530

    chore(config): beautified config

--- lua/kiwi/config.lua
+++ lua/kiwi/config.lua
@@ -1,15 +1,13 @@
-local M = {}
-
 local config = {
   path = "",
 }
 
-M.get = function (key)
+config.get = function (key)
   return config[key]
 end
 
-M.set = function (key, value)
+config.set = function (key, value)
   config[key] = value
 end
 
-return M
+return config
